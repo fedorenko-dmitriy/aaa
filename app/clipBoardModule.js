f5868a90f957e48b97b06efe669a761f3dd28576
Dmutro Fedorenko <pioner@MacBook-Pro-Dmutro.local>
2015-08-08 12:10:38 +0300

    refactor #1

--- app/clipBoardModule.js
+++ app/clipBoardModule.js
@@ -4,69 +4,72 @@
 
 /*Default obj = {id:'string', value: 'string'}*/
 
-var TempClipBoard = function(){
-    this.tempClipBoard = [];
-}
-
-TempClipBoard.prototype.toClipBoard = function(elem){
-    var self = this;
-    var copyObj = {
-        id: $(elem).attr("id"),
-        value: $(elem).closest(".form-group").find("input").val()
+var tempClipBoard = (function(){
+    var TempClipBoard = function(){
+        this.tempClipBoard = [];
     }
-    var flag = this._checkMatchOnTempClipBoard(copyObj);
-    console.log(copyObj.id);
-    this._manipulationWithTempClipBoard(flag, copyObj);
-    this._manipulationWithButtonClass(flag, copyObj);
 
-    return this._copyToClipBoard();
-}
-
-TempClipBoard.prototype._checkMatchOnTempClipBoard = function(copyObj) {
-    var self = this;
-    var flag = false;
-    $.each(self.tempClipBoard, function(key, item){
-
-        if(item.value == copyObj.value){
-            flag = true;
-            return false;
+    TempClipBoard.prototype.toClipBoard = function(elem){
+        var self = this;
+        var copyObj = {
+            id: $(elem).attr("id"),
+            value: $(elem).closest(".form-group").find("input").val()
         }
-    });
-    return flag;
-};
+        var flag = this._checkMatchOnTempClipBoard(copyObj);
+        console.log(copyObj.id);
+        this._manipulationWithTempClipBoard(flag, copyObj);
+        this._manipulationWithButtonClass(flag, copyObj);
 
-TempClipBoard.prototype._manipulationWithTempClipBoard = function (flag, copyObj) {
-    var self = this;
-    if (flag) {
-        var array = [];
-        $.each(self.tempClipBoard, function (key, item) {
-            if (item.value !== copyObj.value) {
-                array.push(item);
+        return this._copyToClipBoard();
+    }
+
+    TempClipBoard.prototype._checkMatchOnTempClipBoard = function(copyObj) {
+        var self = this;
+        var flag = false;
+        $.each(self.tempClipBoard, function(key, item){
+
+            if(item.value == copyObj.value){
+                flag = true;
+                return false;
             }
         });
-        self.tempClipBoard = array;
-    } else {
-        self.tempClipBoard.push(copyObj);
+        return flag;
+    };
+
+    TempClipBoard.prototype._manipulationWithTempClipBoard = function (flag, copyObj) {
+        var self = this;
+        if (flag) {
+            var array = [];
+            $.each(self.tempClipBoard, function (key, item) {
+                if (item.value !== copyObj.value) {
+                    array.push(item);
+                }
+            });
+            self.tempClipBoard = array;
+        } else {
+            self.tempClipBoard.push(copyObj);
+        }
     }
-}
 
-TempClipBoard.prototype._manipulationWithButtonClass = function(flag, copyObj){
-    if(flag){
-        $("button#"+copyObj.id).removeClass("addedToClipBoard");console.log("class del")
-    } else {
-        $("button#"+copyObj.id).addClass("addedToClipBoard");console.log("class add")
-    }
+    TempClipBoard.prototype._manipulationWithButtonClass = function(flag, copyObj){
+        if(flag){
+            $("button#"+copyObj.id).removeClass("addedToClipBoard");console.log("class del")
+        } else {
+            $("button#"+copyObj.id).addClass("addedToClipBoard");console.log("class add")
+        }
 
-};
+    };
 
-TempClipBoard.prototype._copyToClipBoard = function(){
-    var self = this;
-    var stringToClipBoard = "";
-    $.each(self.tempClipBoard, function(key, item){
-        stringToClipBoard += item.value+"\n";
-        console.log(item.value)
-    });
-    return stringToClipBoard;
-};
+    TempClipBoard.prototype._copyToClipBoard = function(){
+        var self = this;
+        var stringToClipBoard = "";
+        $.each(self.tempClipBoard, function(key, item){
+            stringToClipBoard += item.value+"\n";
+            console.log(item.value)
+        });
+        return stringToClipBoard;
+    };
 
-var tempClipBoard = new TempClipBoard();
+    return new TempClipBoard();
+
+});
--- app/eventBusModule.js
+++ app/eventBusModule.js
@@ -2,113 +2,116 @@
  * Created by pioner on 23.07.15.
  */
 
-var EventBus = function(){
-    //this.hasFlash = false;
-    this.hasFlash = Settings.hasFlash;
-}
 
-EventBus.prototype.addElem = function(){
-    this.addEventToElem();
-}
+var eventBus = (function(){
+    var EventBus = function(){
+        this.hasFlash = Settings.hasFlash;
+    }
 
-EventBus.prototype.addEventToElem = function(){
-    addZeroClipboard.call(this);
-    appendCombobox();
-    appendAffix();
-}
+    EventBus.prototype.addElem = function(){
+        this.addEventToElem();
+    }
 
-function addZeroClipboard(){
+    EventBus.prototype.addEventToElem = function(){
+        addZeroClipboard.call(this);
+        appendCombobox();
+        appendAffix();
+    }
 
-    if(this.hasFlash){
-        var TCBvariable;
+    function addZeroClipboard(){
 
-        $("button.init-btn").each(function(key, elem){
-            if($(this).hasClass("select init-btn")){
-                var selectZC = new ZeroClipboard(this);
-                selectZC.on( "ready", function( readyEvent ) {
-                    console.log( "ZeroClipboard SWF is ready! " +selectZC.id);
-                    selectZC.on("beforeCopy",function() {
-                        //Do nothing...
+        if(this.hasFlash){
+            var TCBvariable;
+
+            $("button.init-btn").each(function(key, elem){
+                if($(this).hasClass("select init-btn")){
+                    var selectZC = new ZeroClipboard(this);
+                    selectZC.on( "ready", function( readyEvent ) {
+                        console.log( "ZeroClipboard SWF is ready! " +selectZC.id);
+                        selectZC.on("beforeCopy",function() {
+                            //Do nothing...
+                        });
+
+                        selectZC.on("copy",function(event) {
+                            TCBvariable = tempClipBoard.toClipBoard(event.target);
+                            event.clipboardData.setData('text/plain', TCBvariable);
+                        });
+
+                        selectZC.on("afterCopy", function() {
+                            var time = new Date().valueOf();
+                            popUpWindows.showPopup(TCBvariable, time);
+                        });
                     });
+                } else {
 
-                    selectZC.on("copy",function(event) {
-                        TCBvariable = tempClipBoard.toClipBoard(event.target);
-                        event.clipboardData.setData('text/plain', TCBvariable);
-                    });
+                    var copyZC = new ZeroClipboard(this);
+                    copyZC.on( "ready", function( readyEvent ) {
+                        console.log("ZeroClipboard SWF is ready! " + copyZC.id);
+                        copyZC.on("beforeCopy", function () {
+                            // Do nothing...
+                        });
 
-                    selectZC.on("afterCopy", function() {
-                        var time = new Date().valueOf();
-                        popUpWindows.showPopup(TCBvariable, time);
+                        copyZC.on("copy", function (event) {
+                            TCBvariable = $(event.target).closest(".form-group").find("input").val();
+                            event.clipboardData.setData('text/plain', TCBvariable);
+                        });
+
+                        copyZC.on("afterCopy", function () {
+                            var time = new Date().valueOf();
+                            popUpWindows.showPopup(TCBvariable, time);
+                        });
                     });
+                }
+                $(this).removeClass("init-btn");
+            });
+        }
+        else{
+            $("button").hide();
+            $(".form-group .input")
+                .removeClass("col-md-7")
+                .addClass("col-md-11");
+        }
+    }
+
+    function appendCombobox(){
+        $("select").each(function(key, elem){
+            if($(this).attr("display") != "none" && !$(this).hasClass("combobox")){
+                $(this).combobox();
+                $(this).addClass("combobox");
+
+
+                $(this).on("change", function(event){
+                    var target = $(event.target);
+                    var option = target.val();
+                    if(option){
+                        var newClass = option.split(".").join("\\.");
+                        target.parents(".block").find(".folder").removeClass("active");
+                        $("div[class='folder "+newClass+"']").addClass("active");
+                    }
                 });
-            } else {
 
-                var copyZC = new ZeroClipboard(this);
-                copyZC.on( "ready", function( readyEvent ) {
-                    console.log("ZeroClipboard SWF is ready! " + copyZC.id);
-                    copyZC.on("beforeCopy", function () {
-                        // Do nothing...
-                    });
-
-                    copyZC.on("copy", function (event) {
-                        TCBvariable = $(event.target).closest(".form-group").find("input").val();
-                        event.clipboardData.setData('text/plain', TCBvariable);
-                    });
-
-                    copyZC.on("afterCopy", function () {
-                        var time = new Date().valueOf();
-                        popUpWindows.showPopup(TCBvariable, time);
-                    });
+                $('.bs-docs-sidebar').affix({
+                    offset: {
+                        top: $('.bs-docs-sidebar').offset().top-20
+                    }
                 });
             }
-            $(this).removeClass("init-btn");
         });
     }
-    else{
-        $("button").hide();
-        $(".form-group .input")
-            .removeClass("col-md-7")
-            .addClass("col-md-11");
-    }
-}
 
-function appendCombobox(){
-    $("select").each(function(key, elem){
-        if($(this).attr("display") != "none" && !$(this).hasClass("combobox")){
-            $(this).combobox();
-            $(this).addClass("combobox");
-
-
-            $(this).on("change", function(event){
-                var target = $(event.target);
-                var option = target.val();
-                if(option){
-                    var newClass = option.split(".").join("\\.");
-                    target.parents(".block").find(".folder").removeClass("active");
-                    $("div[class='folder "+newClass+"']").addClass("active");
-                }
-            });
-
+    function appendAffix(){
+        if(!$('.bs-docs-sidebar') && (!$('.bs-docs-sidebar').hasClass(".affix") || !$('.bs-docs-sidebar').hasClass(".affix-top"))){
             $('.bs-docs-sidebar').affix({
                 offset: {
                     top: $('.bs-docs-sidebar').offset().top-20
                 }
             });
         }
-    });
-}
-
-function appendAffix(){
-    if(!$('.bs-docs-sidebar') && (!$('.bs-docs-sidebar').hasClass(".affix") || !$('.bs-docs-sidebar').hasClass(".affix-top"))){
-        $('.bs-docs-sidebar').affix({
-            offset: {
-                top: $('.bs-docs-sidebar').offset().top-20
-            }
-        });
     }
-}
 
 
-var eventBus = new EventBus();
+    return new EventBus();
+})();
+
 
 
